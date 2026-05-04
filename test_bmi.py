8b6daae013c187692703e15318ff5ece501f3018
DylanChoo <DYLANCHOOTL.25@ichat.sp.edu.sg>
2026-05-04 15:30:00 +0800

    week 3

---
+++ test_bmi.py
@@ -0,0 +1,14 @@
+import Lab2.bmi as bmi
+
+
+def test_FRIDGEHOARDEROVERWEIGHT():
+    result = bmi.calculate_bmi(1.73, 110)
+    assert (result == 1)
+
+def test_youresafefornow():
+    result = bmi.calculate_bmi(1.73, 57)
+    assert (result == 0)
+
+def test_youreBONES():
+    result = bmi.calculate_bmi(1.73, 40)
+    assert (result == -1)
